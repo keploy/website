f23047dc2ba54bd7ef2a0a3f81f6067bbfa3261f
Aditya-eddy <aditya282003@gmail.com>
2024-03-31 10:24:46 +0530

    adding padding
    Signed-off-by: Aditya-eddy <aditya282003@gmail.com>

--- components/TwitterTestimonials.tsx
+++ components/TwitterTestimonials.tsx
@@ -127,7 +127,7 @@ const TwitterTestimonials = () => {
     <>
       <div className="relative mt-20 mb-20">
         <div className=" relative mt-2 mb-8 z-10 max-w-5xl mx-auto flex flex-col justify-center">
-          <h3 className=" text-center h2 text-secondary-300">
+          <h3 className=" text-center h2 px-10 text-secondary-300">
           We love when users talk about Keploy..
           </h3>
           {isMobile ? (
