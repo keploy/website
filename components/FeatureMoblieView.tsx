052909cb4951472841174c1ae3252b66ce1960d8
Siddharth Baleja <siddharthbaleja83@gmail.com>
2024-07-26 15:06:16 +0530

    Fixes issue
    Signed-off-by: Siddharth Baleja <siddharthbaleja83@gmail.com>

--- components/FeatureMoblieView.tsx
+++ components/FeatureMoblieView.tsx
@@ -276,7 +276,7 @@ export default function FeaturesMobileView() {
         >
           <div>
             <Link
-              className="w-full mb-4 btn text-secondary-300 bg-primary-300 hover:font-semibold mt-300 sm:w-auto sm:mb-0"
+              className="w-full mb-4 btn text-secondary-300 bg-primary-300 hover:font-semibold mt-100 sm:w-auto sm:mb-0"
               href="https://keploy.io/docs"
             >
               Documentation{' '}
