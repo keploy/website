2351ab91dc7c9dc7b7b462f5d334b304e96bcef1
Yaten Dhingra <yaten598@gmail.com>
2024-07-28 13:24:15 +0530

    added Platform section to Keploy's navbar

---
+++ components/nav/navItemWithLargeDropdown.tsx
@@ -0,0 +1,180 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import { UpIcon } from "./UpIcon";
+import DownIcon from "./DownIcon";
+import Image from "next/image";
+
+export interface NavItemWithLargeDropdownProps {
+  heading: string;
+  dropdownProductData: DropdownProductdataInterface[];
+  dropdownSolutionsData: DropdownProductdataInterface[];
+  dropdownSocialsData: DropdownProductdataInterface[];
+}
+export interface DropdownProductdataInterface {
+  heading: string;
+  links: LinkDatainterface[];
+}
+export interface LinkDatainterface {
+  pagelink: string;
+  pageName: string;
+  pageSvg: string;
+}
+
+export default function NavItemWithLargeDropdown({
+  heading,
+  dropdownProductData,
+  dropdownSolutionsData,
+  dropdownSocialsData,
+}: NavItemWithLargeDropdownProps) {
+  const [openDropdown, setShowDropdown] = useState(false);
+  const showDropdown = () => {
+    setShowDropdown(true);
+  };
+  const hideDropdown = () => {
+    setShowDropdown(false);
+  };
+  const toggleDropdown = () => {
+    setShowDropdown(!openDropdown);
+  };
+
+  return (
+    <div
+      onMouseEnter={showDropdown}
+      onMouseLeave={hideDropdown}
+      className="relative flex items-center py-3 transition duration-150 ease-in-out cursor-pointer"
+    >
+      <div className="flex align-center text-gray-600">
+        <div>
+          <div className="font-medium text-gray-600 px-3 py-3 flex items-center transition duration-150 ease-in-out whitespace-nowrap relative group">
+            {heading}
+            <span className="absolute left-0 bottom-0 w-full h-[.195rem] bg-current transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-out"></span>
+          </div>
+        </div>
+        <div onClick={toggleDropdown} className="pt-4">
+          {openDropdown ? (
+            <UpIcon className="text-current" />
+          ) : (
+            <DownIcon className="text-current" />
+          )}
+        </div>
+      </div>
+      {openDropdown && (
+        <div className="fixed top-20 left-0 bg-[#eeeded] pb-8 z-10 w-full h-[400px]">
+          <div className="flex justify-evenly items-start">
+            <div className="flex items-center justify-evenly">
+              {openDropdown && (
+                <div className="">
+                  <div>
+                    {dropdownProductData.map((lists) => (
+                      <div key={lists.heading}>
+                        {" "}
+                        <span className="text-[#666666] uppercase px-3 py-3 flex items-center transition duration-150 ease-in-out flex-col text-[1.25rem]">
+                          {lists.heading}
+                          <span className="absolute left-0 bottom-0 w-full h-[.125rem] bg-white"></span>
+                        </span>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="flex flex-row gap-5">
+                    {dropdownProductData.map((lists) => (
+                      <ul>
+                        {lists.links.map((links) => (
+                          <li className="flex flex-col gap-4 hover:bg-[#E5905E] px-6 py-4 hover:opacity-65">
+                            <Link
+                              href={links.pagelink}
+                              target="_blank"
+                              className="flex"
+                            >
+                              {links.pageSvg && (
+                                <img src={links.pageSvg} className="pr-2" />
+                              )}
+                              {links.pageName}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
+            <div className="flex items-center justify-evenly">
+              {openDropdown && (
+                <div className="">
+                  <div>
+                    {dropdownProductData.map((lists) => (
+                      <div key="Solutions">
+                        {" "}
+                        <span className="text-[#666666] uppercase px-3 py-3 flex items-center transition duration-150 ease-in-out flex-col text-[1.25rem]">
+                          Solutions
+                          <span className="absolute left-0 bottom-0 w-full h-[.125rem] bg-white"></span>
+                        </span>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="flex flex-row gap-5">
+                    {dropdownSolutionsData.map((lists) => (
+                      <ul>
+                        {lists.links.map((links) => (
+                          <li className="flex flex-col gap-4 hover:bg-[#E5905E] px-6 py-4 hover:opacity-65">
+                            <Link
+                              href={links.pagelink}
+                              target="_blank"
+                              className="flex"
+                            >
+                              {links.pageSvg && (
+                                <img src={links.pageSvg} className="pr-2" />
+                              )}
+                              {links.pageName}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
+            <div className="flex items-center justify-evenly">
+              {openDropdown && (
+                <div className="">
+                  <div>
+                    {dropdownProductData.map((lists) => (
+                      <div key="Solutions">
+                        {" "}
+                        <span className="text-[#666666] uppercase px-3 py-3 flex items-center transition duration-150 ease-in-out flex-col text-[1.25rem]">
+                          Socials
+                          <span className="absolute left-0 bottom-0 w-full h-[.125rem] bg-white"></span>
+                        </span>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="flex flex-row gap-5">
+                    {dropdownSocialsData.map((lists) => (
+                      <ul>
+                        {lists.links.map((links) => (
+                          <li className="flex flex-col gap-4 hover:bg-[#E5905E] px-6 py-4 hover:opacity-65">
+                            <Link
+                              href={links.pagelink}
+                              target="_blank"
+                              className="flex"
+                            >
+                              {links.pageSvg && (
+                                <img src={links.pageSvg} className="pr-2" />
+                              )}
+                              {links.pageName}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
--- components/utils/resources.tsx
+++ components/utils/resources.tsx
@@ -1,48 +1,83 @@
-export const PillarPages = [
-    {
-      heading: "Use Cases",
-      links: [
-        {
-          pagelink: "/test-data-generator",
-          pageName: "Test Data Generation"
-        },
-        {
-          pagelink: "/test-case-generator",
-          pageName: "Test Case Generation"
-        },
-        {
-          pagelink: "/unit-test-generator",
-          pageName: "Unit Test Generation"
-        },
-        {
-          pagelink: "/code-coverage",
-          pageName: "Code Coverage" 
-        },
-        {
-          pagelink: "/continuous-integration-testing",
-          pageName: "CI Testing"
-        },
-        {
-          pagelink: "/regression-testing",
-          pageName: "Regression Testing"
-        },
-        {
-          pagelink: "/regression-testing",
-          pageName: "Regression Testing"
-        },
-        {
-          pagelink: "/api-testing",
-          pageName: "API Testing",
-        },
-        {
-          pagelink: "/ai-code-generation",
-          pageName: "AI Code Generation"
-        },
-        {
-          pagelink: "/contract-testing",
-          pageName: "Contract Testing"
-        }
-      ]
-    }
-  ];
-  
+export const ProductList = [
+  {
+    heading: "Product",
+    links: [
+      {
+        pagelink: "https://keploy.io/docs/keploy-explained/introduction/",
+        pageName: "What is Keploy?",
+        pageSvg: "keploy-logo-dropDown.svg",
+      },
+      {
+        pagelink: "https://keploy.io/docs/keploy-explained/why-keploy/",
+        pageName: "USP",
+        pageSvg: "keploy-logo-dropDown.svg",
+      },
+      {
+        pagelink: "https://keploy.io/docs/keploy-explained/how-keploy-works/",
+        pageName: "How Keploy works?",
+        pageSvg: "keploy-navbar-question-mark.svg",
+      },
+      {
+        pagelink: "https://keploy.io/docs/keploy-cloud/cloud-installation/",
+        pageName: "Enterprise edition",
+        pageSvg: "keploy-enterprise-dropDown.svg",
+      },
+    ],
+  },
+];
+
+export const SolutionsList = [
+  {
+    heading: "Solutions",
+    links: [
+      {
+        pagelink: "https://keploy.io/api-testing",
+        pageName: "API testing",
+        pageSvg: "keploy-api-testing-dropDown.svg",
+      },
+      {
+        pagelink: "https://keploy.io/regression-testing",
+        pageName: "Regression Testing",
+        pageSvg: "keploy-api-testing-dropDown.svg",
+      },
+      {
+        pagelink: "https://keploy.io/continuous-integration-testing",
+        pageName: "Continuous Integration Testing",
+        pageSvg: "keploy-api-testing-dropDown.svg",
+      },
+      {
+        pagelink: "https://keploy.io/unit-test-generator",
+        pageName: "Unit test generation",
+        pageSvg: "keploy-api-testing-dropDown.svg",
+      },
+      {
+        pagelink: "https://keploy.io/ai-code-generation",
+        pageName: "AI Code Generation",
+        pageSvg: "keploy-ai-code-generation-dropDown.svg",
+      },
+    ],
+  },
+];
+
+export const SocialsList = [
+  {
+    heading: "Socials",
+    links: [
+      {
+        pagelink: "https://www.linkedin.com/company/keploy/",
+        pageName: "LinkedIn",
+        pageSvg: "keploy-linkedin-dropdown.svg",
+      },
+      {
+        pagelink: "https://github.com/keploy/keploy",
+        pageName: "GitHub",
+        pageSvg: "keploy-github-dropdown.svg",
+      },
+      {
+        pagelink: "https://x.com/Keployio",
+        pageName: "Twitter",
+        pageSvg: "keploy-twitter-dropdown.svg",
+      },
+    ],
+  },
+];
