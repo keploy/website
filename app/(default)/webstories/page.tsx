3d8cf318938735ba678c690f3bc2689a64c36b97
Neha Gupta <gneha21@yahoo.in>
2024-08-01 15:03:14 +0530

    chore: add bg color and margin to hero
    Signed-off-by: Neha Gupta <gneha21@yahoo.in>

--- app/(default)/webstories/page.tsx
+++ app/(default)/webstories/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import StoriesCards from "@/components/webstories/StoriesCards";
-import { DataFiles } from "../../../components/utils/data";
+import { DataFiles } from "@/components/utils/data";
 import Header from "@/components/webstories/components/Header";
 import RootLayout from "@/app/layout";
 
@@ -23,7 +23,7 @@ const Page = () => {
 
   return (
     <RootLayout metadata={metadata}> 
-      <div className="flex flex-row justify-end my-2 mt-10 border  min-h-screen overflow-hidden">
+      <div className="flex flex-row justify-end my-2 mt-10 border bg-neutral-100 min-h-screen overflow-hidden">
         <StoriesCards data={Data.reverse()} />
       </div>
    </RootLayout>
--- components/webstories/StoriesCards.tsx
+++ components/webstories/StoriesCards.tsx
@@ -49,11 +49,9 @@ const StoriesCards = ({ data }: StoriesCardsProps) => {
   };
 
   return (
-    <div className="relative bg-neutral-100 w-full" data-aos="fade-up">
+    <div className="relative bg-neutral-100 w-full my-28" data-aos="fade-up">
       <Container>
-        <h1
-          className="lg:text-5xl text-5xl text-secondary-300 font-extrabold leading-tighter tracking-tighter my-4 text-center"
-        >
+        <h1 className=" lg:text-5xl text-5xl text-secondary-300 font-extrabold leading-tighter tracking-tighter my-6 text-center">
           Test{" "}
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-gradient-500 to-gradient-200">
             Stories
