ada9cf4febf05a60f19626eb8a7de299005a6a81
Aditya-eddy <aditya282003@gmail.com>
2024-09-30 15:03:36 +0530

    correcting-cta
    Signed-off-by: Aditya-eddy <aditya282003@gmail.com>

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -140,11 +140,11 @@ export default function Header() {
             </div>
             
             <Link
-              href="https://forms.gle/waYcLSASm9dfE9tC9"
+              href="https://app.keploy.io/signin"
               target="_blank"
               className="btn-sm text-gray-200 bg-secondary-300  hover:text-primary-300 ml-3"
             >
-              <span>Join Waitlist</span>
+              <span>Sign Up</span>
               {/*<svg className="w-3 h-3 fill-current   hover:text-primary-300 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">*/}
               {/*  <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />*/}
               {/*</svg>*/}
