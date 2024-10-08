e05f92aa412ed5af16c84c7b0899b4c4f147e972
Yaten Dhingra <yaten598@gmail.com>
2024-10-08 19:58:40 +0530

    added docs section in navbar

---
+++ components/nav/docsSection.tsx
@@ -0,0 +1,10 @@
+export default function Docs() {
+  return (
+    <>
+      <div className="font-medium text-gray-600 px-3 py-3 flex items-center transition duration-150 ease-in-out whitespace-nowrap relative group">
+        Docs
+        <span className="absolute text-[#666666] text-[3px] left-[.15rem] bottom-[.15rem] w-full h-[.195rem] bg-current transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-out"></span>
+      </div>
+    </>
+  );
+}
