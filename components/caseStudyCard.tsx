d4bda4446754fecffb2ac65284e2c9a6c2e04ec7
gouravkrosx <gouravgreatkr@gmail.com>
2024-12-08 18:23:17 +0530

    chore: revert css classes
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>

--- components/caseStudyCard.tsx
+++ components/caseStudyCard.tsx
@@ -49,9 +49,9 @@ export default function CaseStudyCard({
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             >
               <path d="m9 18 6-6-6-6" />
             </svg>
